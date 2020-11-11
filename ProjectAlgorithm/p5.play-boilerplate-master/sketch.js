8bfb5e2eb9990186f84533c9fb42f76e51d2a085
kritika03 <43034288+kritika03@users.noreply.github.com>
2020-11-11 15:12:20 +0530

    Add files via upload

---
+++ ProjectAlgorithm/p5.play-boilerplate-master/sketch.js
@@ -0,0 +1,32 @@
+var fixedrect,movingrect;
+
+function setup() {
+  createCanvas(800,400);
+ fixedrect= createSprite(400, 200, 50, 50);
+ fixedrect.shapeColor="green";
+ movingrect =createSprite(200,200,20,20);
+ movingrect.shapeColor="green";
+}
+
+function draw() {
+  background(255,255,255);  
+  movingrect.x=World.mouseX;
+  movingrect.y=World.mouseY;
+  console.log(movingrect.x-fixedrect.x);
+
+
+
+  if(movingrect.x - fixedrect.x < fixedrect.width/2+movingrect.width/2 &&
+    fixedrect.x - movingrect.x < fixedrect.width/2+movingrect.width/2){
+    movingrect.shapeColor="blue";
+    fixedrect.shapeColor="blue";
+  }
+  else {
+    movingrect.shapeColor="green";
+    fixedrect.shapeColor="green";
+  }
+
+
+
+  drawSprites();
+}
